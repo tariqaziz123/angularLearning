b7f63e7b32466a38cc5731c2457bce28360f67f8
tariqaziz123 <taziz7628@gmail.com>
2022-06-29 18:03:10 +0530

    setting environment for phonecat application

---
+++ phoneCat/angular-phonecat/app/app.js
@@ -0,0 +1,18 @@
+var phonecatApp = angular.module('phonecatApp', []);
+
+phonecatApp.controller('PhoneListController',function PhoneListController($scope){
+    $scope.phones = [
+        {
+            name: 'Nexus S',
+            snippet: 'Fast just go faster with Nexus S.'
+        },
+        {
+            name: `Motorola XOOM™  with WiFi`,
+            snippet:'The Next, Next Generation Tablet'
+        },
+        {
+            name: 'MOTOROLA XOOM™',
+            snippet:'The Next, Next Generation Tablet'
+        }
+    ];
+});
